df7e91777ea3c8c650e86c722d6e793304e43341
Nagaratna Hegde <nagaratnahegde@Nagaratnas-MacBook-Air.local>
2026-02-23 14:50:07 +0530

    added feed page

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,4 +1,27 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { API_BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+
 const Feed = () => {
+  const dispatch = useDispatch();
+  const feed = useSelector((state) => state.feed);
+  const getFeed = async () => {
+    if (feed) return;
+    try {
+      const res = await axios.get(API_BASE_URL + "/feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res.data));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
   return <></>;
 };
 
